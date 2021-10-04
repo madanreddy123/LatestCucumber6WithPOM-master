a1185631ed0bbbaed39c47825b392b1888829fad
madan <madanreddy319@gmail.com>
2021-10-04 12:42:52 +0100

    fg

---
+++ src/test/java/Static/Base.java
@@ -0,0 +1,16 @@
+package Static;
+
+import org.openqa.selenium.WebDriver;
+
+public class Base
+
+{
+	WebDriver driver;
+	
+	public Base(WebDriver driver) {
+		 
+		this.driver = driver;
+	}
+	
+
+}
